90273fb237128e47308758eee7c269d8f9d05dc5
Meagan Utsey <meagan.utsey@gmail.com>
2019-12-16 02:30:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
+ 
+# => ["inlets"]
+
+class Anagram.new
+ 
+  attr_accessor .match
+ 
+  def initialize(.match)
+    anagram.new = .match
+    [1, 2, 3] == [1, 2, 3]
+=> true
+ 
+[1, 3, 2] == [1, 2, 3]
+=> false
+  end
+end
